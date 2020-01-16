59e068257c5cc8091c9a5c806e22470441042280
maaz1996 <maaz.abdullah111@gmail.com>
2020-01-16 12:55:56 +0530

    feat: active zuul server

--- src/main/java/com/example/zuulnetflix/ZuulnetflixApplication.java
+++ src/main/java/com/example/zuulnetflix/ZuulnetflixApplication.java
@@ -2,11 +2,13 @@ package com.example.zuulnetflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ZuulnetflixApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 
 		SpringApplication.run(ZuulnetflixApplication.class, args);
 	}
