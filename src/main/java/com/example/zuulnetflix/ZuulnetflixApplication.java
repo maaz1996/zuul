09d6b1fc84b76eb8c83cbba9cb53670a8e5c6baa
maaz1996 <maaz.abdullah111@gmail.com>
2020-01-16 12:26:15 +0530

    project init

---
+++ src/main/java/com/example/zuulnetflix/ZuulnetflixApplication.java
@@ -0,0 +1,13 @@
+package com.example.zuulnetflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulnetflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulnetflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zuulnetflix/ZuulnetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zuulnetflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulnetflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
