bacae89f067e4d657863fbb82410e0f4bbb2d987
maaz1996 <maaz.abdullah111@gmail.com>
2020-01-16 12:49:46 +0530

    port active

--- src/main/java/com/example/zuulnetflix/ZuulnetflixApplication.java
+++ src/main/java/com/example/zuulnetflix/ZuulnetflixApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ZuulnetflixApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ZuulnetflixApplication.class, args);
 	}
 
